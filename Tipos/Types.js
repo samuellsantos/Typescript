d6f809d71388617dfacf41c0c2bab4b6a4f3aa05
Samuel Santos <samuka0102128@gmail.com>
2023-04-10 10:33:04 -0300

    First commit

---
+++ Tipos/Types.js
@@ -0,0 +1,25 @@
+"use strict";
+//@ts-check
+//String
+let nome = 'Samuel';
+//Number
+let numero = 14;
+//Boolean
+let dev = true;
+//Array
+let nomes = ['Samuel', 'Samuka'];
+let nomes2 = [
+    'Samuel', 'Santos'
+];
+//The array can take very types, so define it like this
+let names = [
+    'Samuel', 18, 19, 'Santos'
+];
+//OR
+let names2 = [
+    'Samuel', 18, 19, 'Santos'
+];
+//Tuple
+let people = [
+    2004, 'Samuel'
+];
---
+++ Tipos/Types.ts
@@ -0,0 +1,32 @@
+//@ts-check
+//String
+let nome:string = 'Samuel'
+
+//Number
+let numero:number = 14
+
+//Boolean
+let dev:boolean = true
+
+//Array
+let nomes:string[] = ['Samuel', 'Samuka']
+
+let nomes2: Array<string> = [
+  'Samuel', 'Santos'
+]
+
+//The array can take very types, so define it like this
+let names: (string | number)[] = [
+  'Samuel', 18, 19, 'Santos'
+]
+
+//OR
+
+let names2: Array<string | number> = [
+  'Samuel', 18, 19, 'Santos'
+]
+
+//Tuple
+let people: [number, string] = [
+  2004, 'Samuel'
+]
